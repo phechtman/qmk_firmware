5b27a80ae993e20d0e0386ed6cac7fc864235c2f
U-CORP\phechtman <phechtman@LT-5CG8112F61.corp.nexient.com>
2020-01-14 18:48:51 -0500

    changed to grid layout, added enter on left thumb

--- keyboards/preonic/keymaps/ghost/keymap.c
+++ keyboards/preonic/keymaps/ghost/keymap.c
@@ -15,6 +15,7 @@
  */
 
 #include QMK_KEYBOARD_H
+// #include <print.h>
 
 enum preonic_layers {
   _QWERTY,
@@ -37,7 +38,8 @@ enum preonic_keycodes {
   NUMPAD,
   LOWER,
   RAISE,
-  BACKLIT
+  BACKLIT,
+  JS_FUN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,12 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * | Num | Ctrl| GUI  | Alt  |Lower |    Space    |Raise |  Alt | GUI  | Menu | Ctrl |
   * `-----------------------------------------------------------------------------------'
   */
-  [_QWERTY] = LAYOUT_preonic_1x2uC( \
+  [_QWERTY] = LAYOUT_preonic_grid( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL,  TT(_NUMPAD), KC_LGUI, KC_LALT, LOWER,       KC_SPC,       RAISE,    RALT_T(KC_LEFT), RGUI_T(KC_DOWN), KC_UP, RCTL_T(KC_RIGHT)  \
+    KC_LCTL, TT(_NUMPAD), KC_LGUI, KC_LALT, LOWER, KC_ENT, KC_SPC,   RAISE,    RALT_T(KC_LEFT), RGUI_T(KC_DOWN), KC_UP, RCTL_T(KC_RIGHT)  \
   ),
 
 
@@ -184,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+-------------+------+------+------+------+------|
   * |  Esc |      |  Up  |      |      |      |      |   _ |   -  |   +  |   =  | Bksp |
   * |------+------+------+------+------+-------------+------+------+------+------+------|
-  * |  Caps| Left | Down | Right|      |      |      |   {  |   }  |   [  |   ]  |   \  |
+  * |  Caps| Left | Down | Right|JS_FUN|      |      |   {  |   }  |   [  |   ]  |   \  |
   * |------+------+------+------+------+------|------+------+------+------+------+------|
   * | Shift|   |  |      |      |      |      |      |      |      |   ¢  |   Up | Shift|
   * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -194,7 +196,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_preonic_1x2uC( \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
     KC_ESC,  _______, KC_UP,   _______, _______, _______, _______, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  KC_BSPC, \
-    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS, \
+    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, JS_FUN, _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS, \
     KC_LSFT, KC_PIPE, _______, _______, _______, _______, _______, _______, ALGR_T(KC_4), ALGR_T(KC_5), KC_UP,  KC_RSFT, \
     TT(_NUMPAD), KC_LCTL, KC_LGUI, KC_LALT, LOWER,       KC_ENT,       RAISE,   _______, KC_LEFT, KC_DOWN, KC_RIGHT  \
   ),
@@ -205,7 +207,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+-------------+------+------+------+------+------|
   * |  Esc |      |  Up  |      |      |      |      |   -  |   _  |   =  |   +  | Bksp |
   * |------+------+------+------+------+-------------+------+------+------+------+------|
-  * | Caps | Left | Down | Right|      |      |      |   [  |  ]   |   {  |   }  |   |  |
+  * | Caps | Left | Down | Right|JS_FUN|      |      |   [  |  ]   |   {  |   }  |   |  |
   * |------+------+------+------+------+------|------+------+------+------+------+------|
   * | Shift|  \   |      |      |      |      |      |      |      |   ¢  |   Up | Shift|
   * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -215,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_preonic_1x2uC( \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
     KC_ESC,  _______, KC_UP,   _______, _______, _______, _______, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, KC_BSPC, \
-    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE, \
+    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, JS_FUN, _______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE, \
     KC_LSFT, KC_BSLS, _______, _______, _______, _______, _______, _______, _______, ALGR_T(KC_DLR), KC_UP,  KC_RSFT, \
     TT(_NUMPAD), KC_LCTL, KC_LGUI, KC_LALT, LOWER,       KC_ENT,       RAISE,   KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
   ),
@@ -245,6 +247,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+        case JS_FUN:
+          // print("case reached");
+          if (record->event.pressed) {
+            // print("event pressed");
+            SEND_STRING("() => {");
+            // for(int i = 0; i < 4; i++) {
+            //   SEND_STRING(SS_TAP(KC_LEFT));
+            // }
+          }
+          break;
         case QWERTY:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_QWERTY);
@@ -301,19 +313,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
-        // case BACKLIT:
-        //   if (record->event.pressed) {
-        //     register_code(KC_RSFT);
-        //     #ifdef BACKLIGHT_ENABLE
-        //       backlight_step();
-        //     #endif
-        //     PORTE &= ~(1<<6);
-        //   } else {
-        //     unregister_code(KC_RSFT);
-        //     PORTE |= (1<<6);
-        //   }
-        //   return false;
-        //   break;
       }
     return true;
 };
